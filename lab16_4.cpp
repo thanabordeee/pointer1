86669aa24760ea84572c8f9f7b70bed27bcfada6
thanabordeee <jejekup00123@hotmail.co.th>
2023-02-07 16:11:27 +0700

    complete

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,32 @@ int main(){
 	
 	return 0;
 }
+void shuffle(int &a,int &b, int &c, int &d)
+{
+	int x = rand()%4+1;
+	int g,h,j,k;
+	g = a;
+	h = b;
+	j = c;
+	k = d;
+	switch (x)
+	{
+	case 1:  
+		 a=h; b=k; c=g; d=j;
+		break;
+	case 2: 
+		 a=j; b=g; c=k; d=h;
+		break;
+	case 3: 
+		 a=j; b=k; c=h; d=g;
+		break;
+	case 4: 
+		   b=j; c=k; d=h;
+		break;
+	
+	
+	default:
+		break;
+	}
+	
+}
--- lab16_5.cpp
+++ lab16_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *, int *, int *); 
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,11 +11,37 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d);  
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+
+void shuffle(int *a,int *b, int *c, int *d)
+{
+	int x = rand()%4+1;
+	int g = *a,h = *b,j =*c,k=*d;
+	
+	switch (x)
+	{
+	case 1:  
+		 *a=h; *b=k; *c=g; *d=j;
+		break;
+	case 2: 
+		 *a=j; *b=g; *c=k; *d=h;
+		break;
+	case 3: 
+		 *a=j; *b=k; *c=h; *d=g;
+		break;
+	case 4: 
+		   *b=j;*c=k; *d=h;
+		break;
+	
+	
+	default:
+		break;
+	}
+	
+}
